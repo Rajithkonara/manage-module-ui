06d9de77407fbb86ea399abea6c8630c6c9cfc2d
Rajithkonara <rajithkonara@gmail.com>
2017-12-21 17:19:23 +0530

    model for validation and modified message service
    Signed-off-by: Rajithkonara <rajithkonara@gmail.com>

--- ui/src/app/commons/models/common-data-models.ts
+++ ui/src/app/commons/models/common-data-models.ts
@@ -195,3 +195,17 @@ export class API {
     createdBy: string;
 }
 
+export class Payload {
+    valid: string[];
+    invalid: string[];
+    validationRegex: string;
+    prefixGroup: number;
+    digitsGroup: number;
+}
+
+export class MsisdnValidation {
+    payload: Payload;
+    success: string;
+    message: string;
+}
+
